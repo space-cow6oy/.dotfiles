53e3b9d5d9c541256696633cbfa60fac96dde473
Artem Voloshenko <artyomvoloshenko@gmail.com>
2025-05-29 00:11:55 +0300

    surround for erb

--- nvim/lua/plugins/auto_tag.lua
+++ nvim/lua/plugins/auto_tag.lua
@@ -13,9 +13,9 @@ return {
         -- Empty by default, useful if one of the "opts" global settings
         -- doesn't work well in a specific filetype
         per_filetype = {
-          ["html"] = {
-            enable_close = false,
-          },
+          -- ["html"] = {
+          --   enable_close = false,
+          -- },
         },
       })
     end,
