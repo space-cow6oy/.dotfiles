40b22bbf01fd0f5edc9d79a4c68dc4646f0f9413
Artem Voloshenko <artyomvoloshenko@gmail.com>
2026-03-09 22:41:09 +0300

    neogit with dependencies (plenary) added

--- nvim/init.lua
+++ nvim/init.lua
@@ -139,3 +139,11 @@ spider.setup()
 vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>")
 vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>")
 vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>")
+
+
+vim.pack.add({"https://github.com/nvim-lua/plenary.nvim"})
+vim.pack.add({"https://github.com/NeogitOrg/neogit"})
+neogit = require("neogit")
+neogit.setup()
+
+
